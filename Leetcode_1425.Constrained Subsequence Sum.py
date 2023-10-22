5d98b9afe2800e0a87602712e292a0d41a8c0869
MOYINOLORUN PRECIOUS ADEGBIE <116834557+moyinolorunpreciousadegbie@users.noreply.github.com>
2023-10-21 20:41:40 -0500

    Add files via upload

---
+++ Leetcode_1425.Constrained Subsequence Sum.py
@@ -0,0 +1,88 @@
+def column_sum(lst):
+	res=[]
+	for i in range(0,len(lst)):
+		s=0
+		for j in range(0,len(lst[i])):
+			s+=lst[j][i]
+		res.append(s)
+	return res	
+
+def transpose(l1, l2):
+	
+
+	l2 =[[row[i] for row in l1] for i in range(len(l1[0]))]
+	return l2
+			
+def do(A,k):
+
+	if k == 1:
+		return max(A)
+	
+	cn = 0
+	cc = 0
+	for i in range(len(A)):
+		cc += 1
+		if A[i] > 0 :
+			cn+=1
+			#print(cnt)
+			if cn == 1 and cc == len(A):
+				return A[i]
+			 
+	k -=1 
+
+	AC = [[0] * len(A) for i in range(len(A))]
+
+	for jj in range(len(AC) // len(A) ):
+		for i in range(len(A)):
+			for j in range(len(A)):
+				AC[i+  len(A) * jj][j] = A[j]
+
+
+	for i in  range(len(A)):
+		for kk in range(k):
+			cnt = 0
+			for move in range(kk + i +1):
+				if move < len(A)  :
+					AC[ i ][move] = 0
+					cnt+=1
+
+	for i in range(1,len(AC)):
+		for j in range(0,i):
+			AC[i][j] = A[j] 
+		
+	RR = len(A) - k + 1
+	AC = AC[0:RR]
+	l2 = []
+	AC = transpose(AC, l2)
+
+	return  max(column_sum(AC) ) 
+
+
+A  = [10,2,-10,5,20]
+
+nums1 = [-1,-2,-3]
+kk = 1
+
+nums2 = [10,-2,-10,-5,20]
+k = 2
+
+
+print( do(A, k) )
+
+print()
+
+print( do(nums1, kk) )
+
+print()
+
+print( do(nums2, k) )
+
+print()
+
+
+
+numm=[-1000,-2000,-3000,-4000,2] # answer is 2 working on a shorter, better faster solution
+
+ki=2
+
+print( do(numm, ki) )
